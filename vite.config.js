6443f34845a465c4ae7e3c4fb30eba182ed5c4c1
Saurabh Kushwaha <kushwahasaurabh08615@gmail.com>
2026-04-14 14:13:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,      // important for sandbox
+    port: 2222       // match with CodeSandbox port
+  }
 })
